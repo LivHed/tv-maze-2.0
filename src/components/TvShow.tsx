6d3d3f44f996e1187c0887fdbd3b1e97c4ec9223
LivHed <livia.hedin@gmail.com>
2024-10-24 12:01:00 +0200

    Handle go back button and render info about the tv show

--- src/components/TvShow.tsx
+++ src/components/TvShow.tsx
@@ -1,35 +1,46 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import {useEffect, useState } from 'react'
 import axios from 'axios'
 import { TvShow } from './SearchTvShows'
 
-// handle state for one tv show, make axios api call but with only one tv show, that you click on in the search page.
-
-/*interface Params {
-	id: string;
-} */
-
 function TvShowInfo () {
-    const { id } = useParams<{ id: string }>(); // hämta id från urlen
- //   const { id } = useParams<Params{id}>();
-    const [searchResult, setSearchResult] = useState<TvShow | null>(null);
+    const { id } = useParams<{ id: string }>() // hämta id från urlen
+    const [tvShow, setTvShow] = useState<TvShow | null>(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
         axios
         .get(`https://api.tvmaze.com/shows/${id}`)
         .then((response) => {
-            setSearchResult(response.data)
+            setTvShow(response.data)
         })
         .catch((err) => {
             console.log(err.message)
         })
     }, [id])
-    
+
+    const handleGoBack = () => {
+        navigate('/')
+      };
+
+    // If thShow is null or undefined, render the loading div (implement a loading spinner instead)
+    // Else if tvShow is defined (not null or undefined), render the div with the tvshow details.
+    // handles that summary is wrapped in p tags with a regex that removes the p tags and only show the text.
     return (
-    <div>
-			<h1>Tv show info</h1>
-			<p>ID: {id}</p>
-		</div>
+        <div>
+            <button onClick={handleGoBack} className="Go-back-button">Back to search results</button>
+            {!tvShow ? (
+                <div>Loading...</div>
+                ) : (
+                <div  className="Tv-show-info">
+                    <h1 className="TV-show-name">{tvShow.name}</h1>
+                    <img src={tvShow.image?.medium} alt={tvShow.name} />
+                    <div>Genres: {tvShow.genres}</div>
+                    <div>{tvShow.summary.replace(/<\/?[^>]+(>|$)/g, "")}</div>
+                </div>
+                )  
+            }
+        </div>
     )
 }
 
